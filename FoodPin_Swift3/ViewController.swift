9f5664e47d571a673bd92f63fd0d2228c0c3b593
Jerry Chen <andyjeryDevelop@icloud.com>
2016-09-29 23:23:39 +0800

    Beginning

---
+++ FoodPin_Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodPin_Swift3
+//
+//  Created by Jerry Chen on 2016/9/29.
+//  Copyright © 2016年 Jerry Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
